53f012b35f64ab1d0d77181de0dfe0c96f75e718
redVi <liberty195@gmail.com>
2021-08-02 00:21:30 +0700

    refactoring

--- client/src/pages/home.js
+++ client/src/pages/home.js
@@ -1,15 +1,13 @@
 import React from 'react'
 import { useQuery } from '@apollo/client'
 import ReactMarkdown from 'react-markdown'
-import { GET_NOTES } from '../gql/notes'
+import { GET_NOTES } from '../gql/queries/notes'
 
 const Home = () => {
-  const { data, loading, fetchMore } = useQuery(GET_NOTES)
+  const { data, loading, error, fetchMore } = useQuery(GET_NOTES)
 
   if (loading) return <p>Loading</p>
-  if (!data.noteFeed && !data.noteFeed.notes) return <p>No has any note</p>
-
-  console.log('data', data)
+  if (error) return <p>No has any note</p>
 
   return (
     <section>
