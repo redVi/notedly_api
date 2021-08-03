e163c6fa4a2a114f28aeb144624144b429317a64
redVi <liberty195@gmail.com>
2021-08-03 23:16:28 +0700

    fetch more notes

--- client/src/pages/home.js
+++ client/src/pages/home.js
@@ -2,14 +2,41 @@ import React from 'react'
 import { useQuery } from '@apollo/client'
 import { GET_NOTES } from '../gql/queries/notes'
 import NoteFeed from '../components/NoteFeed'
+import Button from '../components/Button'
 
 const Home = () => {
   const { data, loading, error, fetchMore } = useQuery(GET_NOTES)
+  const fetchMoreNotes = () =>
+    fetchMore({
+      variables: {
+        cursor: data.noteFeed.cursor
+      },
+      updateQuery: (previousResult, { fetchMoreResult }) => {
+        return {
+          noteFeed: {
+            cursor: fetchMoreResult.noteFeed.cursor,
+            hasNextPage: fetchMoreResult.noteFeed.hasNextPage,
+            notes: [
+              ...previousResult.noteFeed.notes,
+              ...fetchMoreResult.noteFeed.notes
+            ],
+            __typename: 'noteFeed'
+          }
+        }
+      },
+    })
 
   if (loading) return <p>Loading</p>
   if (error) return <p>No has any note</p>
 
-  return <NoteFeed notes={data.noteFeed.notes} />
+  return (
+    <React.Fragment>
+      <NoteFeed notes={data.noteFeed.notes} />
+      {data.noteFeed.hasNextPage && (
+        <Button onClick={fetchMoreNotes}>Load more</Button>
+      )}
+    </React.Fragment>
+  )
 }
 
 export default Home
